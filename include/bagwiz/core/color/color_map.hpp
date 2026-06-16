37243914e99862f2d5f3b3388493eb62a1fbb6c4
otenim <mineto.tsukada@gmail.com>
2026-06-16 22:27:29 +0900

    feat(color): add jet/turbo/viridis/grayscale/rainbow color maps

--- include/bagwiz/core/color/color_map.hpp
+++ include/bagwiz/core/color/color_map.hpp
@@ -9,10 +9,27 @@
 #ifndef BAGWIZ__CORE__COLOR__COLOR_MAP_HPP_
 #define BAGWIZ__CORE__COLOR__COLOR_MAP_HPP_
 
+#include <array>
+#include <cstdint>
+
 namespace bagwiz::core::color
 {
 
 enum class ColorMapName { kJet, kTurbo, kViridis, kGrayscale, kRainbow };
+
+struct RGB {
+  std::uint8_t r = 0;
+  std::uint8_t g = 0;
+  std::uint8_t b = 0;
+};
+
+struct ColorMap {
+  std::array<RGB, 256> table{};
+};
+
+ColorMap make_color_map(ColorMapName name);
+RGB apply(const ColorMap & map, std::uint8_t index);
+std::uint8_t normalize(float value, float min, float max);
 
 }  // namespace bagwiz::core::color
 
---
+++ test/core/color/color_map_test.cpp
@@ -0,0 +1,46 @@
+// Copyright 2026 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+#include "bagwiz/core/color/color_map.hpp"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+using bagwiz::core::color::apply;
+using bagwiz::core::color::ColorMapName;
+using bagwiz::core::color::make_color_map;
+using bagwiz::core::color::normalize;
+
+TEST(ColorMapTest, JetMapsZeroToBlue)
+{
+  const auto map = make_color_map(ColorMapName::kJet);
+  const auto blue = apply(map, 0);
+  EXPECT_GT(blue.b, blue.r);
+  EXPECT_GT(blue.b, blue.g);
+}
+
+TEST(ColorMapTest, NormalizesRange)
+{
+  EXPECT_EQ(normalize(0.0f, 0.0f, 10.0f), 0);
+  EXPECT_EQ(normalize(10.0f, 0.0f, 10.0f), 255);
+  EXPECT_EQ(normalize(5.0f, 0.0f, 10.0f), 127);
+}
+
+TEST(ColorMapTest, GrayscaleIsMonotonic)
+{
+  const auto map = make_color_map(ColorMapName::kGrayscale);
+  const auto black = apply(map, 0);
+  const auto white = apply(map, 255);
+  EXPECT_EQ(black.r, 0);
+  EXPECT_EQ(white.r, 255);
+  EXPECT_EQ(black.r, black.g);
+  EXPECT_EQ(black.r, black.b);
+}
+
+}  // namespace
